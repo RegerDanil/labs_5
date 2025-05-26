3ab8bf19840718d6ff01e425ea7c705e5527aefc
RegerDanil <danilreger@gmaol.com>
2025-05-26 15:49:22 +0600

    update tests

--- src/test/java/tests/FindPhoneTest.java
+++ src/test/java/tests/FindPhoneTest.java
@@ -5,50 +5,29 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import phones.ByModelTag;
 import phones.FindPhone;
 import phones.Phone;
+import phones.Tag;
 
 public class FindPhoneTest {
 
-    public FindPhoneTest() {}
-
     @Test
-    public void byColorTest() {
-        FindPhone fph=new FindPhone();
-        List<Phone> find=fph.byColor(Color.RED);
-
-        assertEquals(3, find.size());
+    public void testFindByTag() {
+        FindPhone findPhone = new FindPhone();
+        Tag modelTag = new ByModelTag("nokii");
+        List<Phone> phones = findPhone.findByTag(modelTag);
+        for (Phone phone : phones) {
+            System.out.println(phone.toString());
+        }
     }
 
     @Test
-    public void byModelTest() {
-        FindPhone fph=new FindPhone();
-        List<Phone> find=fph.byModel("vokii");
-
-        assertEquals(4, find.size());
-    }
-
-    @Test
-    public void byMSizeTest() {
-        FindPhone fph=new FindPhone();
-        List<Phone> find=fph.byMSize(64);
-
-        assertEquals(2, find.size());
-    }
-
-    @Test
-    public void byModelAndPriceLowTest() {
-        FindPhone fph=new FindPhone();
-        List<Phone> find=fph.byModelAndPriceLow("vokii",13000);
-
-        assertEquals(2, find.size());
-    }
-
-    @Test
-    public void byMSizeAndNotColorTest() {
-        FindPhone fph=new FindPhone();
-        List<Phone> find=fph.byMSizeAndNotColor(8,Color.RED);
-
-        assertEquals(3, find.size());
+    public void testByModelAndPriceLowAndColor() {
+        FindPhone findPhone = new FindPhone();
+        List<Phone> phones = findPhone.byModelAndPriceLowAndColor("nokii", 12000, Color.PINK);
+        for (Phone phone : phones) {
+            System.out.println(phone.toString());
+        }
     }
 }
