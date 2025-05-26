0641843782ce0937ea29d20235d8499f34432cdb
RegerDanil <danilreger@gmaol.com>
2025-05-26 15:47:38 +0600

    update FindPhone

--- src/main/java/phones/FindPhone.java
+++ src/main/java/phones/FindPhone.java
@@ -4,41 +4,29 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class FindPhone {
-    private List<Phone> data=new PhoneDB().getPhoneData();
+    private List<Phone> data = new PhoneDB().getPhoneData();
 
-    public List<Phone> byColor(Color col) {
-        List<Phone> find=new LinkedList<>();
-        for(Phone ph : data)
-            if (ph.getColor()==col)   find.add(ph);
+    public List<Phone> findByTag(Tag tag) {
+        List<Phone> find = new LinkedList<>();
+        for (Phone ph : data) {
+            if (tag.find(ph)) {
+                find.add(ph);
+            }
+        }
         return find;
     }
 
-    public List<Phone> byModel(String mod) {
-        List<Phone> find=new LinkedList<>();
-        for(Phone ph : data)
-            if (ph.getModel().equalsIgnoreCase(mod))   find.add(ph);
-        return find;
-    }
+    public List<Phone> byModelAndPriceLowAndColor(String mod, double price, Color color) {
+        Tag modelTag = new ByModelTag(mod);
+        Tag priceTag = new ByPriceLowTag(price);
+        Tag colorTag = new ByColorTag(color);
 
-    public List<Phone> byMSize(int memSize) {
-        List<Phone> find=new LinkedList<>();
-        for(Phone ph : data)
-            if (ph.getMemorySize()==memSize)   find.add(ph);
-        return find;
-    }
-
-    public List<Phone> byModelAndPriceLow(String mod, double price) {
-        List<Phone> find=new LinkedList<>();
-        for(Phone ph : data)
-            if (ph.getModel().equalsIgnoreCase(mod) &&
-                    ph.getPrice()<price)   find.add(ph);
-        return find;
-    }
-
-    public List<Phone> byMSizeAndNotColor(int memSize, Color col) {
-        List<Phone> find=new LinkedList<>();
-        for(Phone ph : data)
-            if (ph.getMemorySize()==memSize && !(ph.getColor()==col))   find.add(ph);
+        List<Phone> find = new LinkedList<>();
+        for (Phone ph : data) {
+            if (modelTag.find(ph) && priceTag.find(ph) && colorTag.find(ph)) {
+                find.add(ph);
+            }
+        }
         return find;
     }
 }
